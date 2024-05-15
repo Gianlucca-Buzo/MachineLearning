448cc12e6a351d258de5f6324ee3b7306429069a
Gianlucca <gdmbuzo@inf.ufpel.edu.br>
2024-05-14 21:21:19 -0300

    Ajustando plot de residuos

--- RandomForest.py
+++ RandomForest.py
@@ -58,9 +58,10 @@ plt.show()
 residuos = y_test - y_pred
 
 # Plotar os resíduos
-plt.scatter(X_test, residuos)
+plt.scatter(datas_convertidas, residuos)
 plt.axhline(y=0, color='red', linestyle='--')
 plt.xlabel("Data")
 plt.ylabel("Resíduos")
 plt.title("Análise Residual")
+plt.xticks(np.arange(0, len(datas_convertidas), 100), rotation=45)  # Ajustar a frequência e rotação dos rótulos do eixo x
 plt.show()
