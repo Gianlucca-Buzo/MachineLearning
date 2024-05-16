4537283ec7209da268b702d9e51587eaf5a0c683
Gianlucca <gdmbuzo@inf.ufpel.edu.br>
2024-05-16 19:08:07 -0300

    Criando script de RidgeRegression

--- RidgeRegression.py
+++ RidgeRegression.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
-import datetime as dt
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
 
@@ -10,16 +9,17 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     df_btc = pd.read_csv('datasets/coin_Bitcoin_PreProcessado.csv')
 
-    x = df_btc.drop(['Close'],axis=1)
+    x = df_btc.drop(['Close'], axis=1)
     y = np.asarray(df_btc['Close'])
 
-    X_train, X_test, y_train, y_test = train_test_split(x, y, train_size=.5, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, train_size=0.7, random_state=42)
 
-    regressor = LinearRegression()
-    regressor.fit(X_train, y_train)
+    # Aplicando Ridge Regression
+    ridge_regressor = Ridge(alpha=50.0)  # Você pode ajustar o valor de alpha para regularizar mais ou menos
+    ridge_regressor.fit(X_train, y_train)
 
     # Fazendo previsões com o conjunto de teste
-    y_pred = regressor.predict(X_test)
+    y_pred = ridge_regressor.predict(X_test)
 
     # Calculando o erro quadrático médio
     mse = mean_squared_error(y_test, y_pred)
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     print("Coeficiente de determinação (R²):", r2)
 
     # Coeficientes da regressão
-    print("Coeficientes:", regressor.coef_)
+    print("Coeficientes:", ridge_regressor.coef_)
 
     # Plot 1: Valores previstos vs. valores reais
     plt.figure(figsize=(10, 6))
@@ -61,40 +61,9 @@ if __name__ == '__main__':
 
     # Plot 3: Análise Residual
     plt.figure(figsize=(10, 6))
-    plt.scatter(X_test['Date'], residuos, alpha=0.6)
+    plt.scatter(X_test['Date'], residuos, alpha=0.9)
     plt.axhline(y=0, color='red', linestyle='--')
     plt.xlabel("Data")
     plt.ylabel("Resíduos")
     plt.title("Análise Residual")
     plt.show()
-
-
-
-
-
-    # Plotando os valores previstos vs. valores reais
-    # plt.scatter(y_test, regressor.predict(X_test))
-    # plt.xlabel("Valor Real")
-    # plt.ylabel("Valor Previsto")
-    # plt.title("Valores Previstos vs. Valores Reais")
-    # plt.show()
-    #
-    # # Usar a coluna de data para plotar a relação entre a variável de entrada e a variável de saída
-    # plt.scatter(X_test['Date'], y_test, label='Real')
-    # plt.plot(X_test['Date'], regressor.predict(X_test), color='red', label='Previsto')
-    # plt.xlabel("Data")
-    # plt.ylabel("Preço de Fechamento")
-    # plt.title("Regressão Linear")
-    # plt.legend()
-    # plt.show()
-    #
-    # # Calculando os resíduos
-    # residuos = y_test - regressor.predict(X_test)
-    #
-    # # Plotando os resíduos
-    # plt.scatter(X_test['Date'], residuos)
-    # plt.axhline(y=0, color='red', linestyle='--')
-    # plt.xlabel("Data")
-    # plt.ylabel("Resíduos")
-    # plt.title("Análise Residual")
-    # plt.show()
